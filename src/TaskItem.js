896172a3e7d6fbc43a3e6682b57f19278e081881
SimranjeetKaurTussar <simranjeetkaurtussar@gmail.com>
2024-08-25 11:33:38 +0530

    Todo list project

---
+++ src/TaskItem.js
@@ -0,0 +1,20 @@
+// src/TaskItem.js
+import React from 'react';
+
+const TaskItem = ({ task, toggleComplete, deleteTask }) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => toggleComplete(task.id)}
+      />
+      <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+        {task.name}
+      </span>
+      <button onClick={() => deleteTask(task.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default TaskItem;
---
+++ src/TaskList.js
@@ -0,0 +1,20 @@
+// src/TaskList.js
+import React from 'react';
+import TaskItem from './TaskItem';
+
+const TaskList = ({ tasks, toggleComplete, deleteTask }) => {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
